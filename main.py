19582541c782847d87da965ab208468229f4a08e
RamielMyWife <>
2021-04-04 09:33:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+#Ejercicios strings
+#La primera parte se enfocará en tomar un archivo de texto y construir una figura a partir de su contenido, sacando su area posteriormente
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Y la segunda parte, en un traductor del inglés al shakespeareano
